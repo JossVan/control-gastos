b5362da735b58fb464c1314b7b7b9301793e7132
jossvan <vanessapolanco.tech@gmail.com>
2025-02-17 21:29:06 -0600

    list and detail expenbse

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,12 @@ export function formatCurrency(amount: number) {
     currency: "USD",
   }).format(amount);
 }
+
+export function formatDate(date: string) : string{
+  return new Intl.DateTimeFormat("es-ES", {
+   weekday: "long",
+   year: "numeric",
+   month  : "long",
+   day : "numeric",
+  }).format(new Date(date));
+}
